efa54ab288ff5ed2e622ecf120b50daa7b948829
Nils Domrose <nils.domrose@inovex.de>
2018-09-19 22:02:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Simple Openstack Usage Report ###
+
+```pip install -r requirements.txt```
+
+## Basic Usage
+
+```./soure.py -c <cloud-name>```
+
+*cloud-name* is the name of your ```cloud.yaml``` cloud definiton.
+
+## Help
+
+```./sour.py -h                                                         [✓][101s](git)-[master][]
+usage: sour.py [-h] [-v] -c CLOUD [-p PROJECT] [-s START] [-e END]
+
+Process args
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         turn on verbose logging
+  -c CLOUD, --cloud CLOUD
+                        cloud name
+  -p PROJECT, --project PROJECT
+                        project name
+  -s START, --start START
+                        start_time
+  -e END, --end END     end_time
+```
---
+++ sour.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import datetime
+import dateutil.relativedelta
+import shade
+import sys
+
+args = None
+now = datetime.datetime.now()
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def get_cloud(cloudname):
+    cloud = shade.openstack_cloud(cloud=cloudname)
+    return cloud
+
+def get_projects(cloud):
+    projects = cloud.list_projects()
+    project_names = []
+    for project in projects:
+        project_names.append(project.name)
+    return project_names
+
+def get_usage(cloud, projects, end=now, start=now + dateutil.relativedelta.relativedelta(months=-1)):
+    usage = {}
+    for project in projects:
+        if project:
+            usage[project] = cloud.get_compute_usage(project, start, end)
+    return usage
+
+def output(usage):
+    for k,v in usage.items():
+        print("Project: {}, total_hours: {}, total_memory_mb_usage: {}".format(k, v['total_hours'], v['total_memory_mb_usage']))
+
+def main():
+    global args
+    parser = argparse.ArgumentParser(description='Process args')
+    parser.add_argument('-v', '--verbose',
+        help='turn on verbose logging', action="store_true" )
+    parser.add_argument('-c', '--cloud', help='cloud name', required=True)
+    parser.add_argument('-p', '--project', help='project name', required=False)
+    parser.add_argument('-s', '--start', help='start_time', required=False)
+    parser.add_argument('-e', '--end', help='end_time', required=False)
+
+    try:
+        args = parser.parse_args()
+        cloud = get_cloud(args.cloud)
+        if not args.project:
+            projects = get_projects(cloud)
+        else:
+            projects = [args.project]
+        usage = get_usage(cloud, projects)
+        output(usage)
+    except Exception as e:
+        raise Usage(e)
+
+if __name__ == "__main__":
+    sys.exit(main())
